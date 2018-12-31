d3b41ace569fa6e7daa5a61aba27ea3ae085bdaa
chetan1728 <43728159+chetan1728@users.noreply.github.com>
2019-01-01 01:35:10 +0530

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,7 +5,7 @@ import os
 
 
 client = discord.Client()
-hq_inst = hq.HQ()
+hq_inst=hq
 
 @client.event
 async def on_ready():
@@ -22,7 +22,7 @@ async def on_message(message):
     author = message.author
 
     if message.content.startswith('!hq new'):
-        embed = discord.Embed(title="HQ New Helper", desciption="HQ New helper running")
+        embed = discord.Embed(title="hq New Helper", desciption="hq New helper running")
         result_state = ''
        
         # phone authenticate
@@ -32,11 +32,11 @@ async def on_message(message):
             embed.add_field(name="Wrong Phone", value=f'{author.name} check {args_val[2]}')
         else:
             embed.add_field(name="SMS Sent", value=f'{author.name} Check {args_val[2]} for your code')
-            embed.add_field(name="Next Stage", value=f'!hq life <sms_code> <referral username> in chat for you HQ life!')
+            embed.add_field(name="Next Stage", value=f'!hq life <sms_code> <referral username> in chat for you hq life!')
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.startswith('!hq life'):
-        embed = discord.Embed(title="HQ Life Helper", desciption="HQ Life helper running")
+        embed = discord.Embed(title="hq Life Helper", desciption="hq Life helper running")
         result_state = ''
         # phone verifiy
         if len(args_val) < 4:
@@ -64,15 +64,15 @@ async def on_message(message):
 
         await client.send_message(message.channel, embed=embed)
     elif message.content.startswith('!hq stats'):
-        embed = discord.Embed(title="HQ stats", desciption=f"Get HQ user **{args_val[2]}** stats helper")
+        embed = discord.Embed(title="hq stats", desciption=f"Get hq user **{args_val[2]}** stats helper")
         if len(args_val) < 3:
             embed.add_field(name="Error Command", value=f'!hq stats <username>')
         else:
             result_state = hq_inst.get_user(args_val[2])
             if result_state == "error_auth":
-                embed.add_field(name="Error Auth", value=f"Bot have no HQ Auth")
+                embed.add_field(name="Error Auth", value=f"Bot have no hq Auth")
             elif result_state == "error_no_user":
-                embed.add_field(name="Wrong User", value=f"**{args_val[2]}** doesn't exist in HQ Life")
+                embed.add_field(name="Wrong User", value=f"**{args_val[2]}** doesn't exist in hq Life")
             else:
                 embed.add_field(name="Games", value=f"**Played**: {result_state['gamesPlayed']}")
                 embed.add_field(name="Wins", 
@@ -81,13 +81,13 @@ async def on_message(message):
                     value=f"**Weekly**: {result_state['leaderboard']['weekly']['rank']}\n**All Time**:{result_state['leaderboard']['alltime']['rank']}")
         await client.send_message(message.channel, embed=embed)
     elif message.content.startswith('!hq nextgame'):
-        embed = discord.Embed(title="HQ Upcoming Game", desciption=f"HQ Upcoming Game")
+        embed = discord.Embed(title="hq Upcoming Game", desciption=f"hq Upcoming Game")
         info = hq_inst.get_show_info()
         embed.add_field(name="GameInfo", value=f"**DateTime**: {info['nextShowTime']}\n**Prize**: {info['nextShowPrize']}")
         await client.send_message(message.channel, embed=embed)
 
     elif message.content.startswith('!help'):
-        embed = discord.Embed(title="HQ Life Bot", desciption="Help")
+        embed = discord.Embed(title="hq Life Bot", desciption="Help")
         embed.add_field(name="Command", 
             value = "**1.new**: !hq new +<country prefix><number>\n**2.life**:!hq life <code> <referral username>\n**3.life**:!hq stats <username>\n**4.life**:!hq nextgame")
         await client.send_message(message.channel, embed=embed)
